38d8864daca8abccb1d6e940e6d0fb59f5eb656c
VeroPolegre <veronicapolegre98@gmail.com>
2023-10-06 14:31:18 +0200

    localStorage updated

--- main.js
+++ main.js
@@ -8,50 +8,47 @@ const msg2 = document.getElementById("msg2");
 
 const btn = document.getElementById("btn");
 
-let allUsers = JSON.parse(localStorage.getItem("allUsers")) || [];
-function saveUsers(e) {
-  e.preventDefault();
+function saveUsers() {
   const user = {
     name: userName.value,
     email: email.value,
     password1: password1.value,
     password2: password2.value,
   };
-  allUsers.push(user);
-  localStorage.setItem("allUsers", JSON.stringify(allUsers));
-  console.log(allUsers);
+  localStorage.setItem("user", JSON.stringify(user));
 }
-saveUsers();
 
-// function validation1(e) {
-//   e.preventDefault();
-//   const reEmail = /(\w+?@\w+?\x2E.+)/;
-//   if (
-//     name.value == "" ||
-//     email.value == "" ||
-//     password1.value == "" ||
-//     password2.value == ""
-//   ) {
-//     msg.innerText = "Rellena todos los campos";
-//   } else if (reEmail.test(email.value) !== true) {
-//     msg.innerText =
-//       "Por favor inserte un correo correcto que contenga almenos un @";
-//   } else {
-//     msg.innerText = `Formulario enviado con éxito, ${name.value} ${email.value} `;
-//   }
-//   setTimeout(() => {
-//     msg.innerText = "";
-//   }, 3000);
-// }
+function validation1(e) {
+  e.preventDefault();
+  const reEmail = /(\w+?@\w+?\x2E.+)/;
+  if (
+    userName.value == "" ||
+    email.value == "" ||
+    password1.value == "" ||
+    password2.value == ""
+  ) {
+    msg1.innerText = "Rellena todos los campos";
+  } else if (reEmail.test(email.value) !== true) {
+    msg1.innerText =
+      "Por favor inserte un correo correcto que contenga almenos un @";
+  } else {
+    msg1.innerText = `User created succesfully, ${userName.value} ${email.value} `;
+  }
+  setTimeout(() => {
+    msg1.innerText = "";
+  }, 3000);
+}
 
-// function validation2(e) {
-//   e.preventDefault();
-//   if (password1 === password2) {
-//     msg2.innerHTML = "Please repeat the same password";
-//   }
-//   setTimeout(() => {
-//     msg.innerText = "";
-//   }, 3000);
-// }
+function validation2(e) {
+  e.preventDefault();
+  if (password1 !== password2) {
+    msg2.innerHTML = "Please repeat the same password";
+  } else {
+    msg2.innerHTML = "User created succesfully!";
+  }
+  setTimeout(() => {
+    msg2.innerText = "";
+  }, 3000);
+}
 
 btn.addEventListener("click", saveUsers);
