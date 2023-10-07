a1018b9301ab87692dfd2480c7538ca2b9ae0c24
VeroPolegre <veronicapolegre98@gmail.com>
2023-10-07 18:51:31 +0200

    hidden class in css, better returns, added classLists to remove .hidden

--- main.js
+++ main.js
@@ -2,11 +2,14 @@ const userName = document.getElementById("userName");
 const email = document.getElementById("email");
 const password = document.getElementById("password");
 const passwordRepeat = document.getElementById("passwordRepeat");
-const userValidationAlert = document.getElementById("userValidationAlert");
-const passwordValidationAlert = document.getElementById(
-  "passwordValidationAlert"
+const validationAlert = document.getElementById("validationAlert");
+const validationAlertMessage = document.getElementById(
+  "validationAlertMessage"
 );
-const btn = document.getElementById("btn");
+const userValidationSuccessAlert = document.getElementById(
+  "userValidationSuccessAlert"
+);
+const signUpButton = document.getElementById("sign-up-button");
 
 const people = JSON.parse(localStorage.getItem("people")) || [];
 function saveUsers() {
@@ -28,48 +31,51 @@ function usersPrinted() {
 
 function userValidation() {
   const reEmail = /(\w+?@\w+?\x2E.+)/;
-  setTimeout(() => {
-    userValidationAlert.innerText = "";
-  }, 3000);
   if (
-    userName.value == "" ||
-    email.value == "" ||
-    password.value == "" ||
-    passwordRepeat.value == ""
+    userName.value === "" ||
+    email.value === "" ||
+    password.value === "" ||
+    passwordRepeat.value === ""
   ) {
-    userValidationAlert.innerText = "Please, all the fields must be completed";
+    validationAlert.classList.remove("hidden");
+    validationAlertMessage.innerHTML =
+      "Please, all the fields must be completed";
     return false;
-  } else if (reEmail.test(email.value) !== true) {
-    userValidationAlert.innerText = "Please, enter a valid email";
+  } else if (reEmail.test(email.value) === false) {
+    validationAlert.classList.remove("hidden");
+    validationAlertMessage.innerHTML = "Please, enter a valid email";
+
     return false;
   } else {
-    userValidationAlert.innerText = "";
     return true;
   }
 }
 
 function passwordValidation() {
-  setTimeout(() => {
-    passwordValidationAlert.innerText = "";
-  }, 3000);
   if (password.value !== passwordRepeat.value) {
-    passwordValidationAlert.innerHTML = "Please, repeat the same password";
+    validationAlert.classList.remove("hidden");
+    validationAlertMessage.innerHTML = "Please, repeat the same password";
     return false;
   } else {
-    passwordValidationAlert.innerHTML = "";
     return true;
   }
 }
 
 function createUser(e) {
   e.preventDefault();
-
-  const isUserValid = userValidation();
   const isPasswordValid = passwordValidation();
+  const isUserValid = userValidation();
+  setTimeout(() => {
+    validationAlert.classList.add("hidden");
+  }, 3000);
 
   if (isUserValid && isPasswordValid) {
+    validationAlert.classList.add("hidden");
+    userValidationSuccessAlert.classList.remove("hidden");
+    setTimeout(() => {
+      userValidationSuccessAlert.classList.add("hidden");
+    }, 3000);
     saveUsers();
-    userValidationAlert.innerText = "User created successfully!!";
   }
 }
-btn.addEventListener("click", createUser);
+signUpButton.addEventListener("click", createUser);
